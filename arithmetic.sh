06b4c781731734f5d1e9a693f77e45188dd32c3c
mrunali <64697169+mrunali03@users.noreply.github.com>
2020-07-04 17:37:34 +0530

    Sort the result in the Ascending order
    Sort the result in the Ascending order

---
+++ arithmetic.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+echo "Welcome to Sorting Arithmetic Computation Problem"
+echo "Enter Three Number";
+read Number1;
+read Number2;
+read Number3;
+operation1=$(($Number1+$Number2*$Number3));
+operation2=$(($Number1%$Number2+$Number3));
+operation3=$(($Number1+$Number2/$Number3));
+operation4=$(($Number1*$Number2+$Number3));
+
+result[0]="$operation1"
+result[1]="$operation2"
+result[2]="$operation3"
+result[3]="$operation4"
+echo ${result[@]}
+arrayLength=${#result[@]}
+for ((i=0;i<$arrayLength;i++))
+do
+  for ((j=i+1;j<$arrayLength;j++))
+  do
+    if [ ${result[i]} -lt ${result[j]} ];
+    then
+                temp=${result[i]};
+                result[$i]=${result[j]};
+                result[$j]=$temp;
+    fi
+        done
+done
+echo ${result[@]};
+echo "descending Order= "${result[@]}
+
+for ((i=0;i<$arrayLength;i++))
+do
+  for ((j=i+1;j<$arrayLength;j++))
+  do
+    if [ ${result[i]} -gt ${result[j]} ];
+    then
+                temp=${result[i]};
+                result[$i]=${result[j]};
+                result[$j]=$temp;
+    fi
+        done
+done
+echo ${result[@]};
+echo "Ascending Order= "${result[@]}
+
